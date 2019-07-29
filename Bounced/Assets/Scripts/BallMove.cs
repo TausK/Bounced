0d5f724b899ee06aff4ae11c72926cfcbe9b7305
Tausifur <ultimatepow24@gmail.com>
2019-07-29 23:43:55 +1000

    Initial commit

---
+++ Bounced/Assets/Scripts/BallMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    GameObject block;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            Debug.Log("Screen Touched");
+            
+        }
+    }
+}
