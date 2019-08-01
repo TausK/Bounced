c31cb22b2376c582af98798bd8f27f3dd0520f70
Tausifur <ultimatepow24@gmail.com>
2019-08-01 13:14:33 +1000

    Tests

--- Bounced/Assets/Scripts/BallMove.cs
+++ Bounced/Assets/Scripts/BallMove.cs
@@ -2,26 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class BallMove : MonoBehaviour
 {
+    [SerializeField]
     GameObject block;
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    private float deltaX, deltaY;
+    [SerializeField]
+    private Transform origin;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        block = GameObject.FindGameObjectWithTag("DynoBlock");
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        origin.position = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
+        Debug.Log(origin.position);
 
+        //if screen touch is more than no touch then...
+        if (Input.touchCount > 0)
+        {
+            //Retun touch count only as 1 touch
+            Touch touch = Input.GetTouch(0);
+            //set touch dependant on worldspace
             Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-            Debug.Log("Screen Touched");
-            
+
+
+            //set switch statement
+            //Set touch phase. What will happen upon touch?
+            switch (touch.phase)
+            {
+                //first case/ First touch occurence
+                case TouchPhase.Began:
+                    //Set touch position to the position of the block. 
+                    deltaY = touchPos.y - transform.position.y;
+                    break;
+
+                case TouchPhase.Moved:
+                    rb2d.MovePosition(new Vector2(0, touchPos.y - deltaY));
+                    break;
+
+                case TouchPhase.Ended:
+                    block.transform.position = origin.position;
+                    rb2d.velocity = Vector2.zero;
+
+                    break;
+            }
+
         }
     }
 }
