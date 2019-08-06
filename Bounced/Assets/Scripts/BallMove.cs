c0ca1e8c1e1bfbd3f92d48a4473de3a2790e2957
Tausifur <ultimatepow24@gmail.com>
2019-08-06 17:08:09 +1000

    mechanics changes

--- Bounced/Assets/Scripts/BallMove.cs
+++ Bounced/Assets/Scripts/BallMove.cs
@@ -2,65 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class BallMove : MonoBehaviour
 {
-    [SerializeField]
-    GameObject block;
-    private Rigidbody2D rb2d;
-    [SerializeField]
-    private float deltaX, deltaY;
-    [SerializeField]
-    private Transform origin;
+    #region
 
-    private void Awake()
-    {
-        rb2d = GetComponent<Rigidbody2D>();
-        block = GameObject.FindGameObjectWithTag("DynoBlock");
-    }
+    public float forceX;
+    public float forceY;
+
+    Rigidbody2D rb2d;
+
+    #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        origin.position = transform.position;
-
+        //reference rigidbody component
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(origin.position);
 
-        //if screen touch is more than no touch then...
-        if (Input.touchCount > 0)
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Block")
         {
-            //Retun touch count only as 1 touch
-            Touch touch = Input.GetTouch(0);
-            //set touch dependant on worldspace
-            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-
-
-            //set switch statement
-            //Set touch phase. What will happen upon touch?
-            switch (touch.phase)
-            {
-                //first case/ First touch occurence
-                case TouchPhase.Began:
-                    //Set touch position to the position of the block. 
-                    deltaY = touchPos.y - transform.position.y;
-                    break;
-
-                case TouchPhase.Moved:
-                    rb2d.MovePosition(new Vector2(0, touchPos.y - deltaY));
-                    break;
-
-                case TouchPhase.Ended:
-                    block.transform.position = origin.position;
-                    rb2d.velocity = Vector2.zero;
-
-                    break;
-            }
-
+            rb2d.AddForce(new Vector2(forceX, forceY), ForceMode2D.Impulse);
         }
     }
 }
---
+++ Bounced/Assets/Scripts/BlockPush.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BlockPush : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField]
+    GameObject block;
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    private float blockSpeedY;
+   
+    private float posY;
+    [SerializeField]
+    private Transform origin;
+    #endregion
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        block = GameObject.FindGameObjectWithTag("DynoBlock");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origin.position = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TouchSetup();
+    }
+
+    void TouchSetup()
+    {
+        Debug.Log(origin.position);
+
+        //if screen touch is more than no touch then...
+        if (Input.touchCount > 0)
+        {
+            //Retun touch count only as 1 touch
+            Touch touch = Input.GetTouch(0);
+            //set touch dependant on worldspace
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+
+            //set switch statement
+            //Set touch phase. What will happen upon touch?
+            switch (touch.phase)
+            {
+                //first case/ First touch occurence
+                case TouchPhase.Began:
+                    //begin moving block down
+                    rb2d.velocity = Vector2.down;
+                    break;
+
+
+                case TouchPhase.Ended:
+                    rb2d.velocity = Vector2.up * blockSpeedY;
+                    break;
+            }
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Ball")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
